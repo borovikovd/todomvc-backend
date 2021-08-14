0ffcc14d5f9a3b7d741d47dc9a38f497eaa18db5
Denis Borovikov <denis.borovikov@gmail.com>
2021-08-14 14:56:22 +0100

    Spring Starter template

---
+++ src/main/kotlin/io/github/borovikovd/todomvc/TodomvcBackendApplication.kt
@@ -0,0 +1,11 @@
+package io.github.borovikovd.todomvc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TodomvcBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<TodomvcBackendApplication>(*args)
+}
---
+++ src/test/kotlin/io/github/borovikovd/todomvc/TodomvcBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.borovikovd.todomvc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TodomvcBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
